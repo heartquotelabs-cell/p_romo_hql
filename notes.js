eb218dda8b19b4f87b37c66db044b5ef582f81fe
heartquotelabs-cell <heartquotelabs@gmail.com>
2026-05-31 21:34:22 +0500

    Update notes.js

--- notes.js
+++ notes.js
@@ -179,9 +179,10 @@ document.addEventListener('deviceready', async () => {
 /*••••••••••••••••••••*/
 
 const promo = document.getElementById('mypromo');
+promo.style.display ='none';
 const promobanner = document.createElement('div');
 promobanner.style.cssText = 
-'display:flex;align-items:center;justify-content:space-between;padding:5px;background: var(--bg-card);margin: 10px 10px 0;border-radius:12px;gap:12px; border: 1px solid var(--border-light); ';
+'display:none;align-items:center;justify-content:space-between;padding:5px;background: var(--bg-card);margin: 10px 10px 0;border-radius:12px;gap:12px; border: 1px solid var(--border-light); ';
 const left = document.createElement('div');
 left.style.cssText = 'display:flex;align-items:center;gap:12px';
 const img = document.createElement('img');
